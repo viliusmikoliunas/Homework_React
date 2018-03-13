6affef39aed789ed795b8d8cd75c5fe9ce274d21
Linas <linas.sukevicius@gmail.com>
2018-03-13 18:27:38 +0200

    Include styles.css

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import TodoApp from './TodoApp.jsx';
+import './styles.css';
 
 render(<TodoApp/>, document.querySelector('.react-app'));
