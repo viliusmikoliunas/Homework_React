53890947568a382317cabae4fa7c006405b10f17
Vilius Mikoliūnas <vilius.mikoliunas@gmail.com>
2018-03-20 18:36:04 +0200

    Now unfinished tasks have higher priority than completed ones

--- app/TodoApp.jsx
+++ app/TodoApp.jsx
@@ -20,14 +20,14 @@ export default class TodoApp extends React.Component {
     let newId = 'tdi-' + Math.random().toString(36).substring(2, 15);
     this.setState({
       todoItemList: [
-        ...this.state.todoItemList, 
         <TodoItem 
           key={newId}
           id={newId}
           title={this.state.inputValue} 
           deleteItem={this.deleteItem.bind(this)}
           handleItemAfterCheckboxClick={this.handleItemAfterCheckboxClick.bind(this)}
-        />
+        />,
+        ...this.state.todoItemList
       ]    
     });
     this.state.inputValue = '';
@@ -47,16 +47,17 @@ export default class TodoApp extends React.Component {
   }
 
   handleItemAfterCheckboxClick(itemId){
-    let newList = this.state.todoItemList.filter(item => item.props.id !== itemId);
     let item = this.state.todoItemList.filter(item => item.props.id === itemId)[0];
-    let newItem;
+    let toDoTasks = this.state.todoItemList.filter(item => item.props.id !== itemId && item.type === TodoItem);
+    let completedTasks = this.state.todoItemList.filter(item => item.props.id !== itemId && item.type === CompletedItem);
     if (item.type === TodoItem){
-      newItem = <CompletedItem key={item.key} id={item.props.id} title={item.props.title} handleItemAfterCheckboxClick={this.handleItemAfterCheckboxClick.bind(this)}/>
+      completedTasks.push(<CompletedItem key={item.key} id={item.props.id} title={item.props.title} handleItemAfterCheckboxClick={this.handleItemAfterCheckboxClick.bind(this)}/>);
     }
     else{
-      newItem = <TodoItem key={item.key} id={item.props.id} title={item.props.title} deleteItem={this.deleteItem.bind(this)} handleItemAfterCheckboxClick={this.handleItemAfterCheckboxClick.bind(this)}/>
+      toDoTasks.push(<TodoItem key={item.key} id={item.props.id} title={item.props.title} deleteItem={this.deleteItem.bind(this)} handleItemAfterCheckboxClick={this.handleItemAfterCheckboxClick.bind(this)}/>);
     }
-    newList.push(newItem);
+    //this approach makes sure that uncompleted tasks are always higher than completed ones
+    let newList = toDoTasks.concat(completedTasks);
     this.setState(
       {
         todoItemList: newList,
